31d55397e9398ef74bfb35963fde6b5eeff3106a
Katock-Cricket <787288968@qq.com>
2024-12-23 16:48:07 +0800

    引入trl, tranformers, bnb的源码

--- train.py
+++ train.py
@@ -27,7 +27,7 @@ def train():
             gradient_accumulation_steps=4,
             warmup_steps=5,
             # num_train_epochs = 1, # Set this for 1 full training run.
-            max_steps=60,
+            max_steps=1,
             learning_rate=2e-4,
             fp16=not is_bfloat16_supported(),
             bf16=is_bfloat16_supported(),
